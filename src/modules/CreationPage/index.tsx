89a08cab3f14dd9bbd3419593217a2bbe3be2e7a
Abdou Ouahib <ouahibdou@gmail.com>
2022-03-13 20:31:17 +0100

    add toasts to creation page

--- src/modules/CreationPage/index.tsx
+++ src/modules/CreationPage/index.tsx
@@ -1,17 +1,28 @@
 import classNames from "classnames";
 import EmptyState from "components/EmptyState";
+import { toast } from "react-toastify";
 import useNFTMarket from "state/nft-market";
 import useSigner from "state/signer";
-import CreationForm from "./CreationForm";
+import CreationForm, { CreationValues } from "./CreationForm";
 
 const CreationPage = () => {
   const { signer } = useSigner();
   const { createNFT } = useNFTMarket();
 
+  const onSubmit = async (values: CreationValues) => {
+    try {
+      await createNFT(values);
+      toast.success("You'll see your new NFT here shortly. Refresh the page.");
+    } catch (e) {
+      toast.warn("Something wrong!");
+      console.log(e);
+    }
+  };
+
   return (
     <div className={classNames("flex h-full w-full flex-col")}>
       {!signer && <EmptyState>Connect your wallet</EmptyState>}
-      {signer && <CreationForm onSubmit={createNFT} />}
+      {signer && <CreationForm onSubmit={onSubmit} />}
     </div>
   );
 };
