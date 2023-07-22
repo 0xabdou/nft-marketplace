33f26af64ada40b29030f63b74817befc6681479
Yihong Wang <yihong.wang@justeattakeaway.com>
2023-07-22 09:49:26 +0200

    initialise

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,14 +2,14 @@ import "@nomiclabs/hardhat-waffle";
 import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 
-const RINKEBY_URL = process.env.RINKEBY_URL as string;
+const GOERLI_URL = process.env.GOERLI_URL as string;
 const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.11",
   networks: {
-    rinkeby: {
-      url: RINKEBY_URL,
+    goerli: {
+      url: GOERLI_URL,
       accounts: [PRIVATE_KEY],
     },
   },
